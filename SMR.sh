8eebfdd30e69c425577287c47bc2e9c60ccd7d52
Carolina-Toste <82058789+Carolina-Toste@users.noreply.github.com>
2023-12-03 22:22:10 +0000

    Create SMR.sh

---
+++ SMR.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+snakemake -p -s SMR_analysis.smk --use-conda --cluster-config cluster_config.yaml --cluster "qsub -pe smp {cluster.num_cores} -l h_vmem={cluster.maxvmem}" -j 20 $@ 2> SMR.log
+mail -s "SMR finished" $EMAIL < SMR.log
